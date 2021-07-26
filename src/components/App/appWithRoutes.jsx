b35f2cda180a74a1178d0043994d533b1d07ade3
Araik Rasoyan <araikrasoian@gmail.com>
2021-07-26 17:10:36 +0300

    save

--- src/components/App/appWithRoutes.jsx
+++ src/components/App/appWithRoutes.jsx
@@ -3,14 +3,49 @@ import PATH from '../../constants/path';
 import {Route, Switch} from 'react-router-dom';
 import AboutMe from "../../pages/AboutMe";
 import MyProjects from "../../pages/MyProjects";
+import {Box, Container, makeStyles} from "@material-ui/core";
+
+const useStyles = makeStyles(theme => ({
+    mainContainerContent: {
+        display: "flex",
+        justifyContent: "center",
+        padding: 0,
+        [theme.breakpoints.up('xs')]: {
+            width: 200,
+        },
+        [theme.breakpoints.up('sm')]: {
+            width: 400,
+        },
+        [theme.breakpoints.up('md')]: {
+            width: 700,
+        },
+        [theme.breakpoints.up('lg')]: {
+            width: 1000,
+        },
+        [theme.breakpoints.up('xl')]: {
+            width: 1200,
+        },
+    }
+}));
 
 const AppWithRoutes = () => {
+    const classes = useStyles();
+
     return (<>
-        <Switch>
-            <Route path={"PATH.AUTHORIZATION"} render={() => <AboutMe />} />
-            <Route path={"aboutMe"} render={() => <AboutMe />} />
-            <Route path={"myProjects"} render={() => <MyProjects />} />
-        </Switch>
+        <Box className={classes.wrapper}
+             minHeight={"100vh"}>
+            {"HEADER"}
+            <Container
+                className={classes.mainContainerContent}>
+                {"Navigation"}
+                <div style={{flexGrow: 1}}>
+                    <Switch>
+                        <Route path={PATH.ABOUT_ME} render={() => <AboutMe/>}/>
+                        <Route path={PATH.MY_PROJECTS} render={() => <MyProjects/>}/>
+                    </Switch>
+                </div>
+            </Container>
+        </Box>
     </>);
 }
 
