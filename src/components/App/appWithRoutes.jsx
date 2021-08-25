fa6ffa034ad5ca7db6f69d6421aaf3fd00f56637
Araik Rasoyan <araikrasoian@gmail.com>
2021-08-25 16:03:56 +0300

    fix router

--- src/components/App/appWithRoutes.jsx
+++ src/components/App/appWithRoutes.jsx
@@ -4,19 +4,13 @@ import {Route, Switch} from 'react-router-dom';
 import Profile from "../../pages/Profile";
 import Projects from "../../pages/Projects";
 
-
-// const browserHistory = createBrowserHistory();
-// const routingStore = new RouterStore();
-// const history = syncHistoryWithStore(browserHistory, routingStore);
-
 const AppWithRoutes = () => {
 
     return (<>
         <Switch>
             <Route path={PATH.PROFILE} component={() => <Profile/>}/>
-            {/*<Route path={PATH.PROJECTS} history={history} component={projects}/>*/}
             <Route path={PATH.PROJECTS} component={Projects}/>
-            <Route path={PATH.ALL} render={() => <div>всё!</div>}/>
+            <Route path={PATH.ALL} render={() => <Profile/>}/>
         </Switch>
     </>);
 }
