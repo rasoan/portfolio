c16a0bf32f848ac206b60e6adf2a29526099e988
Araik Rasoyan <araikrasoian@gmail.com>
2021-09-01 15:16:01 +0300

    fix my modal

--- src/components/MyModal/MyModal.jsx
+++ src/components/MyModal/MyModal.jsx
@@ -7,8 +7,14 @@ import storeModalWindow from "../../store/storeModalWindow";
 import {observer} from "mobx-react";
 import {AiOutlineClose} from "react-icons/all";
 import {Box, Button, Paper} from "@material-ui/core";
+import storeApp from "../../store/storeApp";
+import clsx from "clsx";
+import {grey, teal} from "@material-ui/core/colors";
 
 const useStyles = makeStyles((theme) => ({
+    darkModeBackground: {
+        backgroundColor: props => props.darkMode ? grey[700] : grey[200]
+    },
     root: {
         border: 0,
         borderRadius: 8,
@@ -19,17 +25,25 @@ const useStyles = makeStyles((theme) => ({
         justifyContent: 'center',
     },
     closeButton: {
+        color: props => props.darkMode ? grey[50] : grey[800],
+        borderRadius: 0,
+        border: "none",
         marginLeft: "auto",
-        display: "block",
-        height: 30,
+        height: 35,
+        minWidth: 35,
+        fontSize: 20,
+        padding: "0 12px",
+        display: "flex",
+    },
+    closeButtonContainer: {
+
     },
     closeButtonIcon: {},
     closeButtonWrapper: {}
 }));
 
 const MyModal = () => {
-    const classes = useStyles();
-
+    const classes = useStyles({darkMode: storeApp.darkMode});
 
     return (
         <div>
@@ -47,12 +61,14 @@ const MyModal = () => {
             >
                 <Fade in={storeModalWindow.isOpen}>
                     <Paper className={classes.root}>
-                        <Button
-                            classes={{root: classes.closeButton}}
-                            onClick={() => storeModalWindow.toggle(false)}
-                        >
-                            <AiOutlineClose className={classes.closeButtonIcon}/>
-                        </Button>
+                        <div className={clsx(classes.closeButtonContainer, classes.darkModeBackground)}>
+                            <Button variant="outlined"
+                                    className={classes.closeButton}
+                                    onClick={() => storeModalWindow.toggle(false)}
+                            >
+                                <AiOutlineClose className={classes.closeButtonIcon}/>
+                            </Button>
+                        </div>
                         {storeModalWindow.content}
                     </Paper>
                 </Fade>
